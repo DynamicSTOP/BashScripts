aacb25f45f5e56729da41efdfbb1aa2ba82acccc
Leonid <DynamicSTOP@users.noreply.github.com>
2018-09-28 17:48:22 +0200

    Update common.sh

--- common.sh
+++ common.sh
@@ -2,3 +2,9 @@
 
 # find all modified in last 30 days and zip it
 tar czvf 30days.tgz `find . -mtime -30 -type f`
+
+# list all files in archive
+tar --list --verbose --file=30days.tgz
+
+# extract files
+tar -xvzf 30days.tgz
