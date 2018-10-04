07bd72fccf8107f7a8661afa6dbc07e31548f92b
Leonid <DynamicSTOP@users.noreply.github.com>
2018-10-04 11:07:15 +0200

    Update video.sh

--- video.sh
+++ video.sh
@@ -2,7 +2,10 @@
 
 VIDFILENAME="~/Desktop/MOV_2766.AVI"
 #cutted
-/home/leonid/ffmpeg-2.2.4-64bit-static/ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -strict experimental -b:a 128k out.mp4
+ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -strict experimental -b:a 128k out.mp4
 
 #cutted and cropped
-/home/leonid/ffmpeg-2.2.4-64bit-static/ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -strict experimental -b:a 128k -filter:v "crop=1920:1040:0:0" outc.mp4
+ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -strict experimental -b:a 128k -filter:v "crop=1920:1040:0:0" outc.mp4
+
+#cut sound ogg
+ffmpeg -i 'video_file.mp4' -ss 00:13:48.00 -t 3.000 -vn -acodec libvorbis out.ogg
