0f550c020fe97d57344011f8721cb7345e092f2f
Leonid <DynamicSTOP@users.noreply.github.com>
2019-01-01 03:42:36 +0200

    Update vpn_checker.sh
    replaced ppp0 with tun0

--- vpn_checker.sh
+++ vpn_checker.sh
@@ -2,10 +2,10 @@
 
 LOG_FILE=/var/log/vpn_boot.log
 DATE_STR=$(date +'%d/%m/%Y %H:%M:%S')
-WIFI_CONNECTION="wlp3s0"
+WIRED_CONNECTION="enp0s31f6"
 #feel in as you pleased
 VYPER_GATEWAYS=(fr1.vpn.goldenfrog.com eu1.vpn.goldenfrog.com dk1.vpn.goldenfrog.com se1.vpn.goldenfrog.com uk1.vpn.goldenfrog.com de1.vpn.goldenfrog.com ch1.vpn.goldenfrog.com lu1.vpn.goldenfrog.com ie1.vpn.goldenfrog.com it1.vpn.goldenfrog.com es1.vpn.goldenfrog.com no1.vpn.goldenfrog.com fi1.vpn.goldenfrog.com cz1.vpn.goldenfrog.com at1.vpn.goldenfrog.com be1.vpn.goldenfrog.com lt1.vpn.goldenfrog.com gr1.vpn.goldenfrog.com)
-VYPER_VPN_CONF=/etc/NetworkManager/system-connections/VyperVPN
+VYPER_VPN_CONF=/etc/NetworkManager/system-connections/VPNgoldenfrog
 
 #attempts limit
 LIMIT=3
@@ -13,14 +13,13 @@ LIMIT_N=0
 
 # by default, i assume there is table 200 is already here > /etc/iproute2/rt_tables
 IP_LOCAL='192.168.0.0/16'
-# external ip
-IP_EXTER='X.X.X.X' 
+#IP_EXTER=`curl ifconfig.co`
 IP_GATE='192.168.1.1'
 
 
-WIFI_STATUS=`nmcli con | grep "${WIFI_CONNECTION}"`
-if [ -z "${WIFI_STATUS}" ]; then
-        echo "${DATE_STR} WIFI IS DEAD";
+WIRED_STATUS=`nmcli con | grep "${WIRED_CONNECTION}"`
+if [ -z "${WIRED_STATUS}" ]; then
+        echo "${DATE_STR} WIRED CONNECTION IS DEAD";
         exit 0;
 fi
 
@@ -48,14 +47,14 @@ reconnect_vpn() {
         fi
 
 
-        if nmcli con up id VyperVPN; then
+        if nmcli con up id VPNgoldenfrog; then
                 echo "VYPER CONNECTED";
                 cat $VYPER_VPN_CONF | grep "gateway"
         else
                 echo "ERROR WHILE CONNECTING TO VYPER";
                 gateway
                 echo "REPLACING GATEWAY TO ${SELECTED_GATEWAY}"
-                sed -i -E "s/gateway=.*$/gateway=${SELECTED_GATEWAY}/g" $VYPER_VPN_CONF
+                sed -i -E "s/remote=.*$/remote=${SELECTED_GATEWAY}/g" $VYPER_VPN_CONF
                 sleep 2
                 reconnect_vpn
         fi
@@ -78,7 +77,7 @@ add_rules() {
 
 # if vpn ISN't default
         #getting vyper ip
-        CURRENT_VYPER_IP=`ip addr show dev ppp0 | grep "inet" | sed  -E "s/[ ]+/ /" | cut -d" " -f3`;
+        CURRENT_VYPER_IP=`ip addr show dev tun0 | grep "inet" | sed  -E "s/[ ]+/ /" | cut -d" " -f3 | cut -d"/" -f1 | head -n 1`;
 
 
         STATUS=`ip rule | grep "${CURRENT_VYPER_IP}"`
@@ -90,9 +89,8 @@ add_rules() {
         ip rule add from $CURRENT_VYPER_IP table 200
         ip rule add to   $CURRENT_VYPER_IP table 200
 
-        ip route add default dev ppp0 table 200
+        ip route add default dev tun0 table 200
 
-        # because otherwise dante for some fucking reason will send dns requests through default network... we don't want that.
         ip route del 8.8.8.8/32
         ip route del 8.8.4.4/32
         ip route add 8.8.8.8/32 via $CURRENT_VYPER_IP
@@ -104,16 +102,19 @@ add_rules() {
         ip route show
         echo "ROUTE TABLE 200:"
         ip route show table 200
+
+        /etc/init.d/danted restart
+        echo "Restarted danted"
 }
 add_rules
 
-start_sockd(){
-        STATUS=`/etc/init.d/sockd status | grep "Active:" | grep "running"`
+start_danted(){
+        STATUS=`/etc/init.d/danted status | grep "Active:" | grep "running"`
         if [ ! -z "${STATUS}" ]; then
-                echo "sockd is running";
+                echo "danted is running";
                 return
         fi
 
-        /etc/init.d/sockd start
+        /etc/init.d/danted start
 }
-start_sockd
+start_danted
