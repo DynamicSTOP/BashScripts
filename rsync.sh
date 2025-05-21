c9432405e8347a43b1f62d61dbc31a9effedc5cb
Leonid Babikov <DynamicSTOP@users.noreply.github.com>
2025-05-21 13:43:57 +0400

    Create rsync.sh

---
+++ rsync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# assuming both folders exists
+# will do recursive copy insides of source folder into destination folder 
+rsync -av /home/username/source_folder/ /media/destination/folder/
