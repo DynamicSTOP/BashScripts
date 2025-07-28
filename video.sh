42bbf7cb1fd9a06ebc8626a89e41862d0bb424a5
Leonid Babikov <DynamicSTOP@users.noreply.github.com>
2025-07-28 12:53:58 +0400

    Update video.sh

--- video.sh
+++ video.sh
@@ -9,3 +9,6 @@ ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -s
 
 #cut sound ogg
 ffmpeg -i 'video_file.mp4' -ss 00:13:48.00 -t 3.000 -vn -acodec libvorbis out.ogg
+
+#cut mp4 without reencode from 1 min to 6 min
+ffmpeg -i big_file.mp4 -ss 00:01:00 -to 00:06:00 -acodec copy -vcodec copy out.mp4
