030f21936da54de3148e4843bed4896888752ba6
Leonid Babikov <DynamicSTOP@users.noreply.github.com>
2026-01-03 03:25:29 +0400

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -2,3 +2,7 @@
 # assuming both folders exists
 # will do recursive copy insides of source folder into destination folder 
 rsync -av /home/username/source_folder/ /media/destination/folder/
+
+#10mb per sec limit
+rsync -av --bwlimit=10M /home/username/source_folder/ /media/destination/folder/
+
