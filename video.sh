beae8b2a728ed54d1897575aaa90a92b41687dd4
Leonid Babikov <DynamicSTOP@users.noreply.github.com>
2026-01-13 11:16:23 +0400

    Update video.sh

--- video.sh
+++ video.sh
@@ -7,16 +7,23 @@ ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -s
 #cutted and cropped
 ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -strict experimental -b:a 128k -filter:v "crop=1920:1040:0:0" outc.mp4
 
+#cut and scale
+ffmpeg -i $VIDFILENAME -ss 00:00:11.000 -t 00:00:15.000 -c:v libx264 -c:a aac -strict experimental -b:a 128k -vf scale="1280:-1" outc.mp4
+
+#scale, change quality, and mute
+ffmpeg -i $VIDFILENAME  -vf scale="1280:-1" -c:v h264_nvenc -cq 30  -an  outc.mp4 
+
 #cut sound ogg
 ffmpeg -i 'video_file.mp4' -ss 00:13:48.00 -t 3.000 -vn -acodec libvorbis out.ogg
 
 #cut mp4 without reencode from 1 min to 6 min
 ffmpeg -i big_file.mp4 -ss 00:01:00 -to 00:06:00 -acodec copy -vcodec copy out.mp4
 
-
-
 # cut first frame 
 ffmpeg -1 -i input.mp4 -frames:v 1 input.first.png
 # cut last frame (almost)
 ffmpeg -sseof -1 -i input.mp4 -frames:v 1 input.last.png
 # reencode in gimp or optipng later 
+
+# get all encoder options
+ffmpeg -h encoder=h264_nvenc
